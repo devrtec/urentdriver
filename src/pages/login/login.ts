974e39dd1fd4486b3b79425d62bc075d2cd88f50
ralphGSC <47863700+ralphGSC@users.noreply.github.com>
2019-04-08 19:25:23 -0300

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-//@IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
 
+  public formTable: FormGroup = new FormGroup({
+    email: new FormControl(null, [
+      Validators.required  
+    ]),  
+    senha: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(6)
+    ])
+  });
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  formSubmit() { 
   }
 
   ionViewDidLoad() {
